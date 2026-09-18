ada3367e1904f1a0a5482216038450bfcf58bf39
Quang Anh <40393971+quanganhbn168@users.noreply.github.com>
2026-09-18 21:14:01 +0700

    Remove unused admin table total prop

--- resources/views/components/admin/table-card.blade.php
+++ resources/views/components/admin/table-card.blade.php
@@ -1,16 +1,22 @@
-@props(['title', 'total' => null])
+@props(['title'])
 
 <section {{ $attributes->class(['card', 'card-outline', 'card-primary', 'admin-table-card']) }}>
     <div class="card-header">
         <h3 class="card-title mb-0">{{ $title }}</h3>
         <div class="card-tools">
-            @isset($tools){{ $tools }}@endisset
+            @isset($tools)
+                {{ $tools }}
+            @endisset
         </div>
     </div>
+
     {{ $slot }}
+
     @isset($footer)
         @if($footer->hasActualContent())
-            <div class="card-footer clearfix">{{ $footer }}</div>
+            <div class="card-footer clearfix">
+                {{ $footer }}
+            </div>
         @endif
     @endisset
 </section>
