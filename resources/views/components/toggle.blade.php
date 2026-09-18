429bedc4231b2eb27f924aa39cac30b839392aa6
Quang Anh <40393971+quanganhbn168@users.noreply.github.com>
2026-09-18 21:14:05 +0700

    Make admin toggles permission aware

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -8,28 +8,40 @@
 ])
 
 @php
-    $toggleId = 'toggle_' . strtolower($model) . '_' . $field . '_' . $id . '_' . Str::random(4);
+    $toggleId = 'toggle_'.strtolower($model).'_'.$field.'_'.$id.'_'.Str::random(4);
+
+    $canUpdate = ! $disabled
+        && \App\Support\AdminIndexRegistry::can($model, 'update');
 @endphp
 
-<div class="form-check form-switch d-inline-block">
-    <input 
-        class="form-check-input toggle-field-switch cursor-pointer" 
-        type="checkbox" 
-        role="switch" 
-        id="{{ $toggleId }}"
-        data-model="{{ $model }}"
-        data-id="{{ $id }}"
-        data-field="{{ $field }}"
-        data-toggle-url="{{ route('admin.common.toggle') }}"
-        aria-label="{{ $label ?: 'Thay đổi '.str_replace('_', ' ', $field) }}"
-        {{ $checked ? 'checked' : '' }}
-        {{ $disabled ? 'disabled' : '' }}
-        {{ $attributes }}
-        style="width: 2.2em; height: 1.1em;"
-    >
-    @if($label)
-        <label class="form-check-label ms-1 cursor-pointer font-weight-bold" for="{{ $toggleId }}">
-            {{ $label }}
-        </label>
-    @endif
-</div>
+@if($canUpdate)
+    <div class="form-check form-switch d-inline-block">
+        <input
+            class="form-check-input toggle-field-switch cursor-pointer"
+            type="checkbox"
+            role="switch"
+            id="{{ $toggleId }}"
+            data-model="{{ $model }}"
+            data-id="{{ $id }}"
+            data-field="{{ $field }}"
+            data-toggle-url="{{ route('admin.common.toggle') }}"
+            aria-label="{{ $label ?: 'Thay đổi '.str_replace('_', ' ', $field) }}"
+            @checked($checked)
+            {{ $attributes }}
+            style="width: 2.2em; height: 1.1em;"
+        >
+
+        @if($label)
+            <label
+                class="form-check-label ms-1 cursor-pointer fw-semibold"
+                for="{{ $toggleId }}"
+            >
+                {{ $label }}
+            </label>
+        @endif
+    </div>
+@else
+    <span class="badge {{ $checked ? 'text-bg-success' : 'text-bg-secondary' }}">
+        {{ $checked ? 'Đang bật' : 'Đang tắt' }}
+    </span>
+@endif
